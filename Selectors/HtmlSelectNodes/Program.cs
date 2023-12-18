d455443a7cefa5f565ffd09065e5c3c973cbf07e
igoradm <igorpetrov42ru@gmail.com>
2023-12-18 23:28:17 +0300

    selectors

---
+++ Selectors/HtmlSelectNodes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HtmlSelectNodes;
+using HtmlAgilityPack;
+using System.Linq;
+class Program
+{
+    const string address_ukit = "https://www.mgkit.org/"; 
+    static HtmlDocument document;
+    static HtmlWeb webPage;
+    static void Main()
+    {
+        webPage = new HtmlWeb();
+        
+        document = new HtmlDocument();
+
+        document = webPage.Load(address_ukit);
+
+        HtmlNodeCollection nodes = document.DocumentNode.SelectNodes("//div");
+        foreach (HtmlNode node in nodes)
+        {
+            System.Console.WriteLine(node.InnerText);
+        }
+    }
+}
