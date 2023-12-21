91eda37a7c3ba75bf10d24066fe0e27b17133c95
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-21 09:12:56 +0300

    Add files via upload

---
+++ Practics/Practic1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Program
+{
+    using HtmlAgilityPack;
+    using System.Net.Http;
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static async Task Main()
+        {
+            HtmlDocument doc = new HtmlDocument();
+            HtmlWeb web = new HtmlWeb();
+            doc = web.Load("https://ria.ru/");
+            HtmlNode node = doc.DocumentNode.SelectSingleNode(@"//*[@id=""content""]/div/div[2]/div[1]/div[1]/div[1]/div[5]/div/div/div/div/div");
+            HtmlNodeCollection collection = node.ChildNodes;
+
+            foreach (HtmlNode a in collection)
+            {
+                System.Console.WriteLine(a.SelectSingleNode("a").InnerText);
+            }
+        }   
+    }
+}
